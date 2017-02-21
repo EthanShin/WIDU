7726da0b4f309d34290a8ce6f41ef39ee60fefe3
EthanShin <avoirom@gmail.com>
2017-02-21 09:11:45 +0900

    Add undo, redo function

--- app/src/main/java/com/example/avoirom/widu/DrawingActivity.java
+++ app/src/main/java/com/example/avoirom/widu/DrawingActivity.java
@@ -11,17 +11,49 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class DrawingActivity extends AppCompatActivity {
+
+    Paper mPaper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawing);
+
+        mPaper = (Paper) findViewById(R.id.paper);
+    }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.btn_undo:
+                mPaper.undo();
+                break;
+            case R.id.btn_redo:
+                mPaper.redo();
+                break;
+        }
+    }
+
+    public static class Vertex{
+        float x;
+        float y;
+        boolean isDraw;
+
+        public Vertex(float x, float y, boolean isDraw){
+            this.x = x;
+            this.y = y;
+            this.isDraw = isDraw;
+        }
     }
 
     public static class Paper extends View {
 
-        Paint paint = new Paint();
+        private Paint paint = new Paint();
+        private ArrayList<Vertex> mShapes = new ArrayList<Vertex>();
+        private ArrayList<Vertex> mRedoShapes = new ArrayList<Vertex>();
+
         Path path = new Path();
 
         float y ;
@@ -29,38 +61,85 @@ public class DrawingActivity extends AppCompatActivity {
 
         public Paper(Context context, AttributeSet attrs) {
             super(context, attrs);
-        }
 
-        protected void onDraw(Canvas canvas) {
-            paint.setStrokeWidth(3);
+            paint.setAntiAlias(true);
             paint.setColor(Color.BLACK);
             paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeJoin(Paint.Join.ROUND);
+            paint.setStrokeWidth(3);
 
-            canvas.drawPath(path, paint);
+            mRedoShapes = new ArrayList<Vertex>();
         }
 
         @Override
         public boolean onTouchEvent(MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                mShapes.add(new Vertex(event.getX(), event.getY(), false));
+                mRedoShapes.add(new Vertex(event.getX(), event.getY(), false));
 
-            x = event.getX();
-            y = event.getY();
+                return true;
+            } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                mShapes.add(new Vertex(event.getX(), event.getY(), true));
+                mRedoShapes.add(new Vertex(event.getX(), event.getY(), true));
+                invalidate();
 
-            switch(event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    path.moveTo(x, y);
-                    break;
-                case MotionEvent.ACTION_MOVE:
-                    x = event.getX();
-                    y = event.getY();
-                    path.lineTo(x, y);
-                    break;
-                case MotionEvent.ACTION_UP:
-                    break;
+                return true;
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+
             }
+            return false;
+        }
 
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+
+            for(int i = 0; i < mShapes.size(); i++) {
+                if(mShapes.get(i).isDraw) {
+                    canvas.drawLine(mShapes.get(i - 1).x, mShapes.get(i - 1).y,
+                            mShapes.get(i).x, mShapes.get(i).y, paint);
+                }
+            }
+        }
+
+        public void undo() {
+            int idx = 0;
+            int mShapesSize = mShapes.size();
+
+            for(idx = mShapesSize - 1; idx >= 0; idx--) {
+                if(mShapes.get(idx).isDraw) {
+                    mShapes.remove(idx);
+                }
+                else {
+                    mShapes.remove(idx);
+                    break;
+                }
+            }
             invalidate();
+        }
 
-            return true;
+        public void redo() {
+            boolean isFirstFalse = true;
+
+            int idx = 0;
+            int mShapesSize = mShapes.size();
+            int mRedoSize = mRedoShapes.size();
+
+            for(idx = mShapesSize; idx < mRedoSize; idx++) {
+                if(mRedoShapes.get(idx).isDraw) {
+                    mShapes.add(mRedoShapes.get(idx));
+                }
+                else {
+                    if(isFirstFalse) {
+                        mShapes.add(mRedoShapes.get(idx));
+                        isFirstFalse = false;
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+            invalidate();
         }
     }
 }
